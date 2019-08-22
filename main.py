1205f38a10bde6be1309c18347a0d2908695be4b
Javos <ram16230@uvg.edu.gt>
2019-08-21 20:08:35 -0600

    ADD generic integral

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import random
 import math
 
-def integral(f):
+def integral(f, d):
     def aproximation(iteration):
         cumalative = 0
+        vars = [0] * d
         for i in range(0, iteration):
-            cumalative = cumalative + f(random.random())
+            for i in range(0, len(vars)):
+                vars[i] = random.random()
+            cumalative = cumalative + f(tuple(vars))
         return cumalative / iteration
     return aproximation
 
-def cuarta(x):
+def cuarta(v):
+    (x, ) = v
     up = math.exp(-(1/x -1) ** 2)
     down = x ** 2
     return 2 * up / down
 
-print(integral(cuarta)(100000))
+def quinta(v):
+    x, y = v
+    up = math.exp(-((1/x) - 1)*(1 + y))
+    down = x**2
+    return ((1/x) - 1) * up / down
+
+print(integral(cuarta, 1)(100000))
+
+print(integral(quinta, 2)(1000000))
